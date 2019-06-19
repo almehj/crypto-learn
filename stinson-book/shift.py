dbfb4464d775dc2a79bb4dde8d8816baed389e65
Hank Alme <Hank.Alme@jhuapl.edu>
2019-06-19 13:08:58 -0400

    Add several more problem writeups. Need to fix solution of 1.7 to use Euler phi-function

--- stinson-book/shift.py
+++ stinson-book/shift.py
@@ -12,8 +12,8 @@ def main():
             text = [ascii_uppercase.index(c) for c in get_text(infile)]
             
             for i in range(len(ascii_uppercase)):
-                shift_text = [(x+i)%len(ascii_uppercase) for x in text]
-                print(" "+''.join([ascii_uppercase[i] for i in shift_text]))
+                shift_text = [(x-i)%len(ascii_uppercase) for x in text]
+                print(ascii_uppercase[i]+" (== %2d): "%(i)+''.join([ascii_uppercase[i] for i in shift_text]))
         
 if __name__ == "__main__":
     main()
